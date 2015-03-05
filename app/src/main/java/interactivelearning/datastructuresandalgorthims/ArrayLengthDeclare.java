44763c0d57b1b15b9c1dec33c9b3255bc9b4d965
mcgraj27 <jessica_mcgrath331@hotmail.com>
2015-03-05 15:08:26 +0000

    Added more stuff
    Added buttons, toasts and input textbox (only accepts numbers)

--- app/src/main/java/interactivelearning/datastructuresandalgorthims/ArrayLengthDeclare.java
+++ app/src/main/java/interactivelearning/datastructuresandalgorthims/ArrayLengthDeclare.java
@@ -7,28 +7,37 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * Created by Richard on 01/03/2015.
- */
 public class ArrayLengthDeclare extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.arraydeclaration);
-        Button button = (Button) findViewById(R.id.arraydeclarationbutton);
+        Button declarationButton = (Button) findViewById(R.id.arraydeclarationbutton);
+        Button initialisationButton = (Button) findViewById(R.id.arrayinitialisationbutton);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        declarationButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(), "You sure showed me!", Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(getApplicationContext(), "You sure showed that declaration button", Toast.LENGTH_SHORT);
                 toast.show();
             }
         });
+
+        initialisationButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "You sure showed that initialisation button", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
         //TODO write function to out new strings for declaring arrays of set length.
         TextView textViewToChange = (TextView) findViewById(R.id.topic);
         textViewToChange.setText(R.string.array_declaration);
 
         textViewToChange = (TextView) findViewById(R.id.arraydeclaration);
-        textViewToChange.setText(R.string.array_declare_para);
+        textViewToChange.setText(R.string.array_declaration_para);
+
+        textViewToChange = (TextView) findViewById(R.id.arrayinitialisation);
+        textViewToChange.setText(R.string.array_initialisation_para);
     }
 }
